8eae078c910867f91114be3990ee5a0cc83c7ebb
Paola Ayala <paolaaz235@gmail.com>
2022-02-01 20:08:58 -0500

    Creating index js file and editing Eslint
    Creating an index js file and troubleshooting eslint problems

---
+++ index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/*
+
+   whoa! Hold your horses! This is nicely formatted code.
+
+
+*/
+
+const message = 'Hello World';
+
+console.log(message); // this should not show as an error
